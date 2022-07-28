660d3f1147b772e612e6acda8f86d41a2d1f9340
Anikate-De <anikate1de@gmail.com>
2022-07-29 01:45:52 +0530

    Added PNG Icons in place of SVG to increase performance

--- lib/resources/assets.dart
+++ lib/resources/assets.dart
@@ -3,12 +3,10 @@ const String downArrowHeadLottie = 'assets/lottie/down-arrowhead-lottie.json';
 const String logoLottie = 'assets/lottie/logo-lottie.json';
 const String rightArrowHeadLottie = 'assets/lottie/right-arrowhead-lottie.json';
 
-// * Vector SVG Files
-const String logoSVG = 'assets/vectors/logo.svg';
-const String instagramIconSVG = 'assets/vectors/instagram-icon.svg';
-const String githubIconSVG = 'assets/vectors/github-icon.svg';
-
 // * Image Files
 const String homeBGIMG = 'assets/images/home-bg.png';
 const String parallaxBGIMG = 'assets/images/parallax-bg.png';
 const String meIMG = 'assets/images/me.png';
+const String instagramIconIMG = 'assets/images/instagram-icon.png';
+const String githubIconIMG = 'assets/images/github-icon.png';
+const String logoIMG = 'assets/images/logo.png';
